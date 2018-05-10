f6201e745759908dc314c486c0e5e2423b5b7067
Pourliver <pourliver@PourliverHome.local>
2018-05-10 13:16:49 -0400

    Added file upload

--- upload.php
+++ upload.php
@@ -1,13 +1,12 @@
 <?php
 require_once 'connection.php';
 
-if (isset($_POST['upload'])) {
+if (isset($_FILES["upload"]["name"])) {
 
-    $target_dir = "uploads/";
+    $target_dir = __DIR__ . "/uploads/";
     $target_file = $target_dir . basename($_FILES["upload"]["name"]);
     
-    move_uploaded_file($_FILES["upload"]["name"], $target_file);
-    //Save File
+    move_uploaded_file($_FILES["upload"]["tmp_name"], $target_file);
 }
 ?>
 
@@ -19,8 +18,8 @@ if (isset($_POST['upload'])) {
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <form class="login" method="POST">
-        <input type="file" name="upload">
+    <form method="POST" enctype="multipart/form-data">
+        Your file : <input type="file" name="upload">
         <input type="submit" value="Upload File" name="submit">
         <?php
             if (isset($_GET["msg"]) && $_GET["msg"] == 'failed') {
